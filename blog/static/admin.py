63c42e6ac626ff3b7037b5b309a5c406c792e8eb
Narenrohitha <100595686+Narenrohitha@users.noreply.github.com>
2022-03-02 22:32:19 +0530

    Add files via upload

--- blog/static/admin.py
+++ blog/static/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from blog.models import Post
+from django.db import models
+from tinymce.widgets import TinyMCE
 
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	formfield_overrides = {
+		models.TextField: {'widget': TinyMCE()}
+	}
+admin.site.register(Post, PostAdmin)
